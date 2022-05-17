6752e8ee85c3c91f33198ec13dfbc8f9cc9c3157
khald <abv.khallid@mail.ru>
2022-05-17 17:24:57 +0300

    Дополнительное задание

---
+++ Relese2/todo-list.js
@@ -0,0 +1,39 @@
+function createTodoList() {
+  const todoListObject = {
+    items: [{
+        text: "Завершить текущий челлендж",
+        completed: false
+      },
+      {
+        text: "Отдохнуть во время перерыва",
+        completed: false
+      },
+      {
+        text: "Помочь напарнику понять код",
+        completed: false
+      },
+      {
+        text: "Выиграть в мафию",
+        completed: true
+      },
+    ],
+    printAll: function () {
+      for (let i = 0; i < this.items.length; i++) {
+        console.log(this.items[i].text);
+      }
+    },
+    add: function (userText) {
+      this.items.unshift({
+        text: userText,
+        completed: false
+      })
+    },
+    remove: function (rem) {
+      this.items.splice(rem, 1)
+    }
+  }
+
+  return todoListObject;
+}
+
+const todoList = createTodoList();
---
+++ Relese4-5/todo-list.js
@@ -0,0 +1,51 @@
+function createTodoList() {
+  const todoListObject = {
+    items: [{
+        text: "Завершить текущий челлендж",
+        completed: false
+      },
+      {
+        text: "Отдохнуть во время перерыва",
+        completed: false
+      },
+      {
+        text: "Помочь напарнику понять код",
+        completed: false
+      },
+      {
+        text: "Выиграть в мафию",
+        completed: true
+      },
+    ],
+    printAll: function () {
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i].completed == true) {
+          console.log(`[x] ${this.items[i].text}`);
+        } else {
+          console.log(`[ ] ${this.items[i].text}`);
+        }
+      }
+    },
+    add: function (userText) {
+      this.items.unshift({
+        text: userText,
+        completed: false
+      })
+    },
+    remove: function (rem) {
+      this.items.splice(rem, 1)
+    },
+
+    print: function (i) {
+      if (this.items[i].completed == true) {
+        console.log(`[x] ${this.items[i].text}`);
+      } else {
+        console.log(`[ ] ${this.items[i].text}`);
+      }
+    }
+  }
+
+  return todoListObject;
+}
+
+const todoList = createTodoList();
